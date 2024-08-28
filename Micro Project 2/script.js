6fcd46ad5d0d96fc2cebca417aeb4043be077fcf
god-of-mischief <71639365+god-of-mischief@users.noreply.github.com>
2024-08-29 00:57:35 +0530

    Upload HTML, CSS and JS files

---
+++ Micro Project 2/script.js
@@ -0,0 +1,9 @@
+// script.js
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+}
